233526343af52a4201a10b978f33e36d57140c4d
Fernando <fernandoeqp59@gmail.com>
2024-10-28 16:51:28 -0300

    webpack

---
+++ webpack/public/assets/js/bundle.js
@@ -0,0 +1,8 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+document.querySelector("body").innerHTML = "hello";
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
